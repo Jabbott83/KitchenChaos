ea4bcaecab03ecaf0c5cb64f6c87540591e1bb3b
Jabbott83 <jabbott.testing@gmail.com>
2025-01-24 17:16:18 -0600

    Burger Visual on Plate
    Got the plate picking up ingredients and the visuals complete

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -19,6 +19,20 @@ public class ClearCounter : BaseCounter, IKitchenObjectParent {
             //There is a KitchenObject here
             if (player.HasKitchenObject()) {
                 //Player is carrying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    //Player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())) {
+                        GetKitchenObject().DestroySelf();
+                    }
+                } else {
+                    //Player is carrying something other than a plate
+                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject)) {
+                        //Counter is holding a plate
+                        if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO())) {
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
             } else {
                 //Player is not carrying anything
                 GetKitchenObject().SetKitchenObjectParent(player);
